c0227a7990ecb3fdfd5dab6004e4c9a934b7b896
mt <mt>
2014-09-22 01:45:43 +0900

    add worker

---
+++ Worker.hs
@@ -0,0 +1,27 @@
+module Worker
+( startWorker
+) where
+
+import GameOfLife
+import Control.Concurrent.STM (STM, TVar, modifyTVar', atomically)
+import Control.Concurrent (threadDelay, forkIO)
+import Control.Monad (forever, void)
+import Control.Exception (catch, SomeException(..))
+import Debug.Trace (trace)
+
+updateField :: TVar Field -> TVar Int -> STM ()
+updateField field counter =
+    modifyTVar' field nextStep >> modifyTVar' counter (+1)
+
+
+worker :: TVar Field -> TVar Int -> Int -> IO ()
+worker field counter interval = forever $ work `catch` onError
+    where
+    work = do
+        threadDelay interval
+        atomically $ updateField field counter
+
+    onError (SomeException e) = trace (show e) (return ())
+
+startWorker :: TVar Field -> TVar Int -> Int -> IO ()
+startWorker f c i = void . forkIO $ worker f c i
